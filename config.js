33545d72a9422323353f95eb7428b1ef2f967489
Sthumnethmina <131809396+Sthumnethmina@users.noreply.github.com>
2023-05-09 19:34:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['447710173736', 'Owner', true],
-  ['447710173736', 'Ash', true], 
+  ['94757182131', 'Owner', true],
+  ['94757182131', 'Sithum', true], 
   [''] 
 ] //Number of owners
 
